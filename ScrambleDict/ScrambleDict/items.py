3b72b4758b5d6fc64b1d2b4890ef96cf911459ef
nccuSimonLee <simon2020291@gmail.com>
2020-05-28 02:26:03 +0000

    [add] add `URLItem` to `items.py`.

--- ScrambleDict/ScrambleDict/items.py
+++ ScrambleDict/ScrambleDict/items.py
@@ -5,10 +5,7 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
-
-class ScrambledictItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class URLItem(Item):
+    url = Field()
